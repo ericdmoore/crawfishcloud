720e98ac6ff2fd6e7ad6c7a3f3931584a31ad2d3
Eric Moore <eric.d.moore@gmail.com>
2021-04-06 20:55:17 -0500

    Still workking on #1 - this adds debug messages to the CI tests - so as to not print the sensitive values

--- tests/aws.ts
+++ tests/aws.ts
@@ -3,7 +3,6 @@
 import process from 'process'
 import {S3, Credentials, SharedIniFileCredentials} from 'aws-sdk'
 
-
 // @ref https://github.com/ericdmoore/crawfishcloud/settings/secrets/actions to see the keys used
 const key: string | undefined = process.env?.PERSONAL_DEFAULT_AWS_KEY
 const secret: string | undefined = process.env?.PERSONAL_DEFAULT_AWS_SECRET
@@ -12,7 +11,16 @@ const credentials = (key && secret)
     ? new Credentials({accessKeyId:key, secretAccessKey:secret}) 
     : new SharedIniFileCredentials({profile:'personal_default'})
 
-;(key && secret) ? console.log('...using ENV VARS') : console.log('...using AWS Cred file')
+if(key && secret){
+    // CI ENV
+    console.log('...using ENV VARS')
+    console.log('mesage for the ', process.env?.HELLO)
+    console.log( JSON.stringify( process.env?.HELLO, null, 2))
+} else{
+    // Local 
+    console.log('...using AWS Cred file')
+}
+
 const s3c = new S3({credentials})
 
 export default s3c
