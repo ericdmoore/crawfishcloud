9dc081854a2d3a7cd87a940bcc95f6b6090d2a58
Eric Moore <eric.d.moore@gmail.com>
2021-04-06 20:21:45 -0500

    reducer test now paginates too

--- tests/reduce/reducer.test.ts
+++ tests/reduce/reducer.test.ts
@@ -9,7 +9,7 @@ const TIMEOUT = 40 * 1000
 
 test('Find JPGs from the network', async ()=>{
     // aws s3 ls 's3://ericdmoore.com-images/a' --profile='personal_default' | grep 'a*.jpg' | wc -l
-    const count = await crawler({s3c},'s3://ericdmoore.com-images/a*.jpg')
+    const count = await crawler({s3c, maxkeys:15},'s3://ericdmoore.com-images/a*.jpg')
         .reduce(0, asS3, (p) => p+1)
     expect(count).toBe(26)
 },TIMEOUT)
